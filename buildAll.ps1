e9052b83ce966e69ab941bd93dfc6dc788084d35
Sean Hamilton <skhamilt@ucsd.edu>
2014-05-19 08:31:10 -0700

    added automated building, just run buildAll.ps1

---
+++ buildAll.ps1
@@ -0,0 +1,22 @@
+# Build Projects in Order
+cd ../rendering-framework
+MSBuild.exe ./Framework.sln
+cd ../engine
+MSbuild.exe ./engine.sln
+cd ../game
+MSBuild.exe ./Game.sln
+
+# Resources Prep 
+mkdir -Force ../game/Debug/resources
+
+cp ../rendering-framework/Debug/*.dll ../engine/Debug
+cp ../engine/Debug/*.dll ../game/Debug
+
+cp ../rendering-framework/Debug/*.cso ../engine/Debug
+cp ../engine/Debug/*.cso ../game/Debug/resources/
+
+cp ../rendering-framework/SampleApp/*.fbx ../engine/SampleApp/
+cp ../rendering-framework/SampleApp/*.dds ../engine/SampleApp/
+
+cp ../rendering-framework/SampleApp/*.fbx ../game/Debug/resources/
+cp ../rendering-framework/SampleApp/*.dds ../game/Debug/resources/
