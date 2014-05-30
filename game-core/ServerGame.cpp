ef63f74ea6c3744090c736128b773ba62e9a24e9
Jake Maskiewicz <jakemaskiewicz@gmail.com>
2014-05-30 01:29:01 -0700

    No more rbc

--- game-core/ServerGame.cpp
+++ game-core/ServerGame.cpp
@@ -35,33 +35,7 @@ void ServerGame::init() {
 	this->getEngineInstance()->getWorld()->allocateHandle(track, HandleType::GLOBAL);
 	this->getEngineInstance()->getWorld()->insert(track);
 
-	this->populateRedBloodCells();
-
-
 	// HACK --	this only works if the clients are up first, but in the future this will
 	//					only be sent when the game starts after character selection
 	// this->getEngineInstance()->sendEvent( new SoundEvent( static_cast<int>(Sounds::SOUNDTRACK), true, false ));
-}
-
-void ServerGame::populateRedBloodCells() {
-	for (size_t i = 0; i < this->getTrackPath()->nodes.size(); i += 5) {
-		float x = ((float) (rand()) / (float) (INT_MAX));
-		float y = ((float) (rand()) / (float) (INT_MAX));
-		float z = ((float) (rand()) / (float) (INT_MAX));
-
-		Common::Vector4 random(x, y, z);
-		random.normalize();
-		random *= 100.0f; //TODO: radius;
-
-		PathNode n = this->getTrackPath()->nodes[i];
-
-		
-
-		MovingObject* rbc = new MovingObject(ObjectTypes::RedBlood, this, true, false);
-		rbc->setPosition(n.point + random);
-
-		this->getEngineInstance()->getWorld()->allocateHandle(rbc, HandleType::GLOBAL);
-		this->getEngineInstance()->getWorld()->insert(rbc);
-		// todo pick an arbitrary position
-	}
 }
